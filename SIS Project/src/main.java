0fa9ab2cf02e00b6f90898a2338802235414421b
ella.krelovich <ella.krelovich@MU-C1-11.mullen.pvt.k12.co.us>
2022-11-03 13:20:08 -0600

    a

--- SIS Project/src/main.java
+++ SIS Project/src/main.java
@@ -5,9 +5,15 @@ public class main
 		public static void main(String[] args)
 			{
 				System.out.println("Hello World");
+				System.out.println("hi");
+
+				
+				System.out.println("Word");
+				//a
+				
+				System.out.println("Yo");
 				System.out.println("ha");
-				System.out.println("Hi!");  
-				System.out.println("Camille's gonna kill me. Please send help.");
+				System.out.println("Hi!"); 
 
 			}
 
