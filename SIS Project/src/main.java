066e218d3b2743eb7d802b0c8742dff0e21ea961
ella.krelovich <ella.krelovich@MU-C1-11.mullen.pvt.k12.co.us>
2022-11-03 13:17:40 -0600

    HELP

--- SIS Project/src/main.java
+++ SIS Project/src/main.java
@@ -6,7 +6,7 @@ public class main
 			{
 				System.out.println("Hello World");
 				System.out.println("ha");
-				System.out.println("Hi!"); 
+				System.out.println("Hi!");  
 				System.out.println("Camille's gonna kill me. Please send help.");
 
 			}
