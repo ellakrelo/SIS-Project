0f59af0a175863e638d7be50411b684c7eda2530
ella.krelovich <ella.krelovich@MU-C1-11.mullen.pvt.k12.co.us>
2022-11-04 13:00:35 -0600

    Main menu print out added

--- SIS Project/src/MainMenu.java
+++ SIS Project/src/MainMenu.java
@@ -4,16 +4,20 @@ public class MainMenu
 	{
 		static Scanner userIntInput = new Scanner(System.in);
 		static int input = 0;
+		static Scanner userStringInput = new Scanner(System.in);
+		static String pause = "";
 		
 		public static void main(String[] args)
 			{
 				System.out.println("Welcome to the student information system! Press enter to continue.");
-				input = userIntInput.nextInt();
+				pause = userIntInput.nextLine();
 				mainMenu();
 			}
 		
 		public static void mainMenu()
 			{
 				System.out.println("What would you like to do next?\n (1) List Students\n (2) Add or Delete a Student\n (3) Change Grades/Schedule\n (4) Sort Students");
+				input = userIntInput.nextInt();
+				
 			}
 	}
