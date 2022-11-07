7397857e26f038e726d072e5e98e67be12622b2f
owen.farnell <owen.farnell@MU-C1-21.mullen.pvt.k12.co.us>
2022-11-07 13:54:30 -0700

    class uppercase

--- SIS Project/src/SortGPA.java
+++ SIS Project/src/SortGPA.java
@@ -1,6 +1,6 @@
 import java.util.Comparator;
 
-public class sortGPA implements Comparator<Student>
+public class SortGPA implements Comparator<Student>
 	{
 		public int compare(Student s1, Student s2)
 		{
